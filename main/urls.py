c243f25fe6d008904c780c548078c774c1815253
QUILLARD <313st@bk.ru>
2023-11-02 17:14:52 +0600

    update

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import index, other_page, BbLoginView, profile, BbLogoutView, ChangeUserInfoView, ChangeUserPasswordView
 
 app_name = 'main'
 
 urlpatterns = [
+    path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
+    path('accounts/login/', BbLoginView.as_view(), name='login'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/', ChangeUserPasswordView.as_view(), name='password_change'),
 ]
